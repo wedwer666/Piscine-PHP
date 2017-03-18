1534927ff27e925f7cd0bcb22a5d15881b6cf051
wedwer666 <mitriucmaria@gmail.com>
2017-03-18 16:18:17 +0200

    hello

---
+++ day09/ex03 copy/ex02bis/todo.js
@@ -0,0 +1,28 @@
+
+function addLi(targetUl) {
+
+  var inputText = document.getElementById('text').value,
+      li = document.createElement('li'),
+      textNode = document.createTextNode(inputText + ' '),
+      removeButton = document.createElement('button');
+  
+  if (inputText.split(' ').join('').length === 0) {
+    alert('No input');
+    return false;
+  }
+  
+  removeButton.className = 'removeMe';
+  removeButton.innerHTML = ' yep';
+  removeButton.setAttribute('onclick', 'removeMe(this);');
+  
+  li.appendChild(textNode);
+  li.appendChild(removeButton);
+  targetUL=document.getElementById("list");
+  targetUL.innerHTML="<li>"+inputText+"<button class=\"removeMe\" onclick=\"removeMe(this);\"> DONE!</button></li>"+targetUl.innerHTML;
+  
+}
+
+function removeMe(item){
+  var parent = item.parentElement;
+  parent.parentElement.removeChild(parent);
+}
